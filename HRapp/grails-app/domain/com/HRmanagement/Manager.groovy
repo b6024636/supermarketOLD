fdffb84182e9d756c18df42a3192fedbe4833db0
b6024636 <b6024636@my.shu.ac.uk>
2018-03-27 17:47:50 +0100

    added manager ccv

---
+++ HRapp/grails-app/domain/com/HRmanagement/Manager.groovy
@@ -0,0 +1,21 @@
+package com.HRmanagement
+
+class Manager {
+
+String fullName
+String userName
+String password
+String managerEmail
+String office
+String department
+
+    static constraints = {
+	fullName blank:false, nullable:false
+	userName blank:false, nullable:false
+	password blank:false, nullable:false
+	managerEmail blank:false, nullable:false, email: true
+	office blank:false, nullable:false
+	department blank:false, nullable:false
+	
+    }
+}
