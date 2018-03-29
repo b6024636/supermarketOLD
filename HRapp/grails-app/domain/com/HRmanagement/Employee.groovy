ae114c52e8bcc8dcd27a0a6740572d874df89e6f
b6024636 <b6024636@my.shu.ac.uk>
2018-03-29 18:48:55 +0100

    Employees issue fixed, replaced residency with residence and set size to maxSize

--- HRapp/grails-app/domain/com/HRmanagement/Employee.groovy
+++ HRapp/grails-app/domain/com/HRmanagement/Employee.groovy
@@ -14,11 +14,11 @@ String contract
     static constraints = {
 	fullName blank:false, nullable:false
 	dateOfBirth blank:false, nullable:false
-	residency blank:false, nullable:false
+	residence blank:false, nullable:false
 	hourlyRate blank:false, nullable:false
 	employeeID blank:false, nullable:false, unique:true
 	dateEmployed blank:false, nullable:false
-	taxCode blank:false, nullable:false, size:5
+	taxCode blank:false, nullable:false, maxSize:5
 	contract blank:false, nullable:false
     }
 }
